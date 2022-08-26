119e06232e9dd2a0d27407d48d3a15ecb4dcdf1e
Cristian Sotomayor <csotomayorcompsci@gmail.com>
2022-08-25 19:25:45 -0700

    new version

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SotomayorDev.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
