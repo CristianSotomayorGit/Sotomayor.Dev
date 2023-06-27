d06db6ccdf3516a48f5f479f59d54f444f8b7136
csotomayorgit <csotomayorarch@gmail.com>
2023-06-27 00:23:48 -0700

    Test2

--- js/check.js
+++ js/check.js
@@ -26,12 +26,24 @@ function checkWindowSize() {
     // If the window width is 500px or less, load script1.js
     if (width <= 999) {
         loadMScript('js/mainm.js');
+        loadCSS("css/indexm.css");
     } 
     // If the window width is more than 500px, load script2.js
     else {
         loadDScript('js/main.js');
+		loadCSS("css/index.css");
     }
 }
+
+	function loadCSS(filename){ 
+
+	   var file = document.createElement("link");
+	   file.setAttribute("rel", "stylesheet");
+	   file.setAttribute("type", "text/css");
+	   file.setAttribute("href", filename);
+	   document.head.appendChild(file);
+
+	}
 
 // Call checkWindowSize() when the window is resized
 window.onresize = checkWindowSize;
